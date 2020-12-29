3584b5f696d2a1051bf832a442fbea8bc36c0ed7
Mahdi Noori <nekorawesh@gmail.com>
2020-12-29 11:45:03 +0300

    adding maestro_cpx configs

---
+++ board/teknotelelectronics/maestro-cpx/gen-swupdate-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BOARD_DIR=$(dirname $0)
+
+cp ${BOARD_DIR}/sw-description ${BINARIES_DIR}
+
+IMG_FILES="sw-description rootfs.ext4.gz"
+
+pushd ${BINARIES_DIR}
+for f in ${IMG_FILES} ; do
+	echo ${f}
+done | cpio -ovL -H crc > buildroot.swu
+popd
