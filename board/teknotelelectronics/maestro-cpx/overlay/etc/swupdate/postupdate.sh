47c32ea5099ccc437a0fd31d6bef49169b5c0d76
Mahdi Noori <nekorawesh@gmail.com>
2021-01-07 15:59:10 +0300

    merged emmc updates

--- board/teknotelelectronics/maestro-cpx/overlay/etc/swupdate/postupdate.sh
+++ board/teknotelelectronics/maestro-cpx/overlay/etc/swupdate/postupdate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PART_STATUS=$(sgdisk -A 2:get:2 /dev/mmcblk1)
-if test "${PART_STATUS}" = "2:2:1" ; then
-	NEXT_ROOTFS=/dev/mmcblk1p3
+PART_STATUS=$(sgdisk -A 4:get:2 /dev/mmcblk0)
+if test "${PART_STATUS}" = "4:2:1" ; then
+	NEXT_ROOTFS=/dev/mmcblk0p5
 else
-	NEXT_ROOTFS=/dev/mmcblk1p2
+	NEXT_ROOTFS=/dev/mmcblk0p4
 fi
 
 # Add update marker
@@ -12,5 +12,5 @@ mount ${NEXT_ROOTFS} /mnt
 touch /mnt/update-ok
 umount /mnt
 
-sgdisk -A 2:toggle:2 -A 3:toggle:2 /dev/mmcblk1
+sgdisk -A 4:toggle:2 -A 5:toggle:2 /dev/mmcblk0
 reboot
